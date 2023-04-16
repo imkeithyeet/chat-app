598a054c1daa1c921be98ab1ccf17bbf0c8fb8a8
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-04-16 00:49:37 -0700

    done

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import WebSocket, {WebSocketServer} from 'ws'
 
-const wss = WebSocketServer({port:3000})
+const wss = new WebSocketServer({port:3000})
+
+wss.on("connection",function connection(ws){
+    ws.on("message",function message(message){
+        const data = JSON.parse(message)
+
+        if(data.type === "message"){
+            wss.clients.forEach((client)=>{
+                if (client !== ws && client.readyState === WebSocketServer.OPEN){
+                    client.send(JSON.stringify({type: "message", data: data.data}))
+                }
+            })
+        }
+
+    })
+
+})
